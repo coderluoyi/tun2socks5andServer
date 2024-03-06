e266f1823cecb7add5d16ca04a8820479d3f0d6f
罗一 <794593170@qq.com>
2024-03-06 08:51:32 +0800

    实现Wintun设备的读写，及其链路层endpoint的处理方法 Attach 方法

---
+++ wintun/endpoint.go
@@ -0,0 +1,151 @@
+package wintun
+
+import (
+	"context"
+	"errors"
+	"io"
+	"sync"
+
+	"gvisor.dev/gvisor/pkg/buffer"
+	"gvisor.dev/gvisor/pkg/tcpip"
+	"gvisor.dev/gvisor/pkg/tcpip/header"
+	"gvisor.dev/gvisor/pkg/tcpip/link/channel"
+	"gvisor.dev/gvisor/pkg/tcpip/stack"
+)
+
+const (
+	// Queue length for outbound packet, arriving for read. Overflow
+	// causes packet drops.
+	defaultOutQueueLen = 1 << 10
+)
+
+// 基于 Tun (io.ReadWriter) 实现 stack.LinkEndpoint
+// tcpip 协议栈将在 LinkEndpoint 上建立 ipv4.endpoint / tcp.endpoint
+type TunEndpoint struct {
+	*channel.Endpoint
+	
+	tun io.ReadWriter
+
+	// mtu (maximum transmission unit) is the maximum size of a packet.
+	mtu uint32
+
+	// offset can be useful when perform TUN device I/O with TUN_PI enabled.
+	offset int
+
+	// once is used to perform the init action once when attaching.
+	once sync.Once
+
+	// wg keeps track of running goroutines.
+	wg sync.WaitGroup
+}
+
+// New returns stack.LinkEndpoint(.*Endpoint) and error.
+func NewTunEndpoint(tun io.ReadWriter, mtu uint32, offset int) (*TunEndpoint, error) {
+	if mtu == 0 {
+		return nil, errors.New("MTU size is zero")
+	}
+
+	if tun == nil {
+		return nil, errors.New("RW interface is nil")
+	}
+
+	if offset < 0 {
+		return nil, errors.New("offset must be non-negative")
+	}
+
+	return &TunEndpoint{
+		Endpoint: channel.New(defaultOutQueueLen, mtu, ""),
+		tun:      tun,
+		mtu:      mtu,
+		offset:   offset,
+	}, nil
+}
+
+// Attach launches the goroutine that reads packets from io.Reader and
+// dispatches them via the provided dispatcher.
+func (e *TunEndpoint) Attach(dispatcher stack.NetworkDispatcher) {
+	e.Endpoint.Attach(dispatcher)
+	e.once.Do(func() {
+		ctx, cancel := context.WithCancel(context.Background())
+		e.wg.Add(2)
+		go func() {
+			e.outboundLoop(ctx)
+			e.wg.Done()
+		}()
+		go func() {
+			e.dispatchLoop(cancel)
+			e.wg.Done()
+		}()
+	})
+}
+
+func (e *TunEndpoint) Wait() {
+	e.wg.Wait()
+}
+
+// dispatchLoop dispatches packets to upper layer.
+func (e *TunEndpoint) dispatchLoop(cancel context.CancelFunc) {
+	// Call cancel() to ensure (*Endpoint).outboundLoop(context.Context) exits
+	// gracefully after (*Endpoint).dispatchLoop(context.CancelFunc) returns.
+	defer cancel()
+
+	offset, mtu := e.offset, int(e.mtu)
+
+	for {
+		data := make([]byte, offset+mtu)
+
+		n, err := e.tun.Read(data)
+		if err != nil {
+			break
+		}
+
+		if n == 0 || n > mtu {
+			continue
+		}
+
+		if !e.IsAttached() {
+			continue /* unattached, drop packet */
+		}
+
+		pkt := stack.NewPacketBuffer(stack.PacketBufferOptions{
+			Payload: buffer.MakeWithData(data[offset : offset+n]),
+		})
+
+		switch header.IPVersion(data[offset:]) {
+		case header.IPv4Version:
+			e.InjectInbound(header.IPv4ProtocolNumber, pkt)
+		case header.IPv6Version:
+			e.InjectInbound(header.IPv6ProtocolNumber, pkt)
+		}
+		pkt.DecRef()
+	}
+}
+
+// outboundLoop reads outbound packets from channel, and then it calls
+// writePacket to send those packets back to lower layer.
+func (e *TunEndpoint) outboundLoop(ctx context.Context) {
+	for {
+		pkt := e.ReadContext(ctx)
+		if pkt.IsNil() {
+			break
+		}
+		e.writePacket(pkt)
+	}
+}
+
+// writePacket writes outbound packets to the io.Writer.
+func (e *TunEndpoint) writePacket(pkt *stack.PacketBuffer) tcpip.Error {
+	defer pkt.DecRef()
+
+	buf := pkt.ToBuffer()
+	defer buf.Release()
+	if e.offset != 0 {
+		v := buffer.NewViewWithData(make([]byte, e.offset))
+		_ = buf.Prepend(v)
+	}
+
+	if _, err := e.tun.Write(buf.Flatten()); err != nil {
+		return &tcpip.ErrInvalidEndpointState{}
+	}
+	return nil
+}
---
+++ wintun/wintun.go
@@ -0,0 +1,92 @@
+package wintun
+
+import (
+	"fmt"
+	"io"
+	"sync"
+
+	"golang.zx2c4.com/wireguard/tun"
+)
+
+const (
+	offset     = 0
+	defaultMTU = 0 // nt.MTU() 获取 mtu
+)
+
+type Tun struct {
+	*TunEndpoint
+
+	nt     *tun.NativeTun
+	mtu    uint32
+	name   string
+	offset int
+
+	rSizes []int
+	rBuffs [][]byte
+	wBuffs [][]byte
+	rMutex sync.Mutex
+	wMutex sync.Mutex
+}
+
+// Wintun 实现 io.ReadWriter 接口
+var _ io.ReadWriter = (*Tun)(nil)
+// Read(p []byte)
+// Write(p []byte)
+
+func (t *Tun) Read(packet []byte) (int, error) {
+	t.rMutex.Lock()
+	defer t.rMutex.Unlock()
+	t.rBuffs[0] = packet
+	_, err := t.nt.Read(t.rBuffs, t.rSizes, t.offset)
+	return t.rSizes[0], err
+}
+
+func (t *Tun) Write(packet []byte) (int, error) {
+	t.wMutex.Lock()
+	defer t.wMutex.Unlock()
+	t.wBuffs[0] = packet
+	return t.nt.Write(t.wBuffs, t.offset)
+}
+
+func Open(name string, mtu uint32) (_ io.ReadWriter, err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			err = fmt.Errorf("open tun: %v", r)
+		}
+	}()
+
+	t := &Tun{
+		name:   name,
+		mtu:    mtu,
+		offset: offset,
+		rSizes: make([]int, 1),
+		rBuffs: make([][]byte, 1),
+		wBuffs: make([][]byte, 1),
+	}
+
+	forcedMTU := defaultMTU
+	if t.mtu > 0 {
+		forcedMTU = int(t.mtu)
+	}
+
+	nt, err := tun.CreateTUN(t.name, forcedMTU)
+	if err != nil {
+		return nil, fmt.Errorf("create tun: %w", err)
+	}
+	t.nt = nt.(*tun.NativeTun)
+
+	tunMTU, err := nt.MTU()
+	if err != nil {
+		return nil, fmt.Errorf("get mtu: %w", err)
+	}
+	t.mtu = uint32(tunMTU)
+
+	ep, err := NewTunEndpoint(t, t.mtu, offset)
+	if err != nil {
+		return nil, fmt.Errorf("create endpoint: %w", err)
+	}
+	t.TunEndpoint = ep
+
+	return t, nil
+}
+
